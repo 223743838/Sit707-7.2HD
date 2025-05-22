02e632ed3732e6766097134e0d2ca97cd67a188f
s223743838 <s223743838@deakin.edu.au>
2025-05-22 12:39:19 +1000

    first commit

---
+++ calculator.test.js
@@ -0,0 +1,13 @@
+const { add, subtract } = require('./calculator');
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test('subtracts 5 - 3 to equal 2', () => {
+  expect(subtract(5, 3)).toBe(2);
+});
+
+test('intentional fail: 2 + 2 to equal 5', () => {
+  expect(add(2, 2)).toBe(5); // Fail
+});
